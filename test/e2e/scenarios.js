d516f2a8a2d10cc833c729767a738df517705d37
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-28 17:57:27 +0000

    completed all e2e tests - all pass

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -7,5 +7,52 @@ describe('cccApp', function() {
     });
   });
 
+  describe('Card list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html#/cards');
+    });
+
+    it('should list cards in order of APR rate, from lowest to highest', function() {
+
+      var cardListApr = element.all(by.repeater('card in cardCtrl.cards').column('card.apr'));
+
+      function getAprRates() {
+        return cardListApr.map(function(elm) {
+          return elm.getText();
+        });
+      }
+
+      expect(getAprRates()).toEqual([
+        "17.4% APR", "18.9% APR", "22.9% APR", "28.2% APR"
+      ]);
+
+    });
+
+  });
+
+  describe('Card detail view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html#/cards');
+      element.all(by.css('.card-header div p')).first().click();
+    });
+
+    it("clicking on a card's name should show the card's image", function() {
+      var cardImageUrl = 'http://localhost:8000/app/img/CARD_315.png';
+      expect(element(by.css('.card-image img')).getAttribute('src')).toMatch(cardImageUrl);
+    });
+
+    it("clicking on a card's name should show the card's description", function() {
+      var cardDescription = "Earn 3% Rewards in all supermarkets until 31st December (0.5% at their petrol stations).";
+      expect(element(by.css('.card-info')).getText()).toEqual(cardDescription);
+    });
+
+    it("clicking on a card's name should show the card's cashback rate", function() {
+      var cardCashback = "£50.10";
+      expect(element(by.css('.cashback-value p')).getText()).toEqual(cardCashback);
+    });
+
+  });
 
 });
