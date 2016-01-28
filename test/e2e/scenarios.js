e8d449acbf49206326bfefaae1b9d3b5d13d753a
Hamza Sheikh <hsheikhm@gmail.com>
2016-01-28 17:10:16 +0000

    passed first feature test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,6 +1,11 @@
-describe('cccApp', function(){
+describe('cccApp', function() {
 
-  
+  it('should redirect index.html to index.html#/cards', function() {
+    browser.get('app/index.html');
+    browser.getLocationAbsUrl().then(function(url) {
+      expect(url).toEqual('/cards');
+    });
+  });
 
 
 });
